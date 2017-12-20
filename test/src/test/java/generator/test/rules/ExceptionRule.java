37589a9559c09c56ac8f65b7913049b0b18d24bb
Cyndi <Cyndi@10.96.28.24>
2017-12-20 21:37:21 +0800

    refactory screenshot

--- test/src/test/java/generator/test/rules/ExceptionRule.java
+++ test/src/test/java/generator/test/rules/ExceptionRule.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
 import java.text.MessageFormat;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -18,6 +20,12 @@ import org.openqa.selenium.TakesScreenshot;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 
+/**
+ * 截圖例外處理
+ * 
+ * @author Cyndi
+ *
+ */
 public class ExceptionRule implements TestRule {
 
 	class BusinessExceptionStatement extends Statement {
@@ -40,27 +48,37 @@ public class ExceptionRule implements TestRule {
 
 				if (driver != null) {
 					try {
-						Calendar now = Calendar.getInstance();
-
+						// 拍照語法
 						File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
+						// com.esun.automation.iphone8.ios11_2.demoCommonUtils.InstructionManualTest
+						// 取最後一個符號之前＝>com.esun.automation.iphone8.ios11_2.demoCommonUtils
 						String packageName = StringUtils.substringBeforeLast(description.getClassName(), ".");
 
+						// com.esun.automation.iphone8.ios11_2.demoCommonUtils
+						// 取最後一個符號之後=>demoCommonUtils
 						packageName = StringUtils.substringAfterLast(packageName, ".");
 
+						// com.esun.automation.iphone8.ios11_2.demoCommonUtils.InstructionManualTest
+						// 取最後一個符號之 後＝>InstructionManualTest
 						String className = StringUtils.substringAfterLast(description.getClassName(), ".");
 
-						Path path = Paths.get("img/" + packageName);
+						// create folder
+						Path path = Paths.get("img" + File.separator + packageName);
 
 						if (!Files.exists(path)) {
 							Files.createDirectories(path);
 						}
+
 						String methodName = description.getMethodName();
 
-						String fileName = MessageFormat.format("{0}-{1}-{2}:{3}.png", className, methodName,
-								now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE));
+						DateFormat df = new SimpleDateFormat("MMddHHmm");
+						Date now = new Date();
+						String dateString = df.format(now);
 
-						FileUtils.copyFile(srcFile, new File(path + "/" + fileName));
+						String fileName = MessageFormat.format("{0}-{1}-{2}.png", className, methodName, dateString);
+
+						FileUtils.copyFile(srcFile, new File(path + File.separator + fileName));
 
 					} catch (Exception ex) {
 						System.out.println(ex);
